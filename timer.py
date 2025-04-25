5de62a6ba90d3521ea3f2b9d81cbbef08b51746d
sheldonkerosene <sheldonkerosene@gmail.com>
2025-04-26 00:21:59 +0200

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,18 @@
+import subprocess
+import json
+import os
+import time
+import os.path
+
+def zero_to_infinity():
+    i = 0
+    while True:
+        yield i
+        i += 1
+
+for y in zero_to_infinity():
+    print(time.strftime("%a, %d %b %Y %H:%M:%S"))
+    time.sleep(300)
+
+
+#os.system("ls -la")
